8848dadb7881e7fe5ef4eede296c97be1c7fe489
igk <igk@sitecore.net>
2019-07-23 16:25:09 +0300

    enkrypted credentials manager and storage

---
+++ lib/SitecoreSSC_SDK/SitecoreSSC_SDK/Utilities/Credentials/SCCredentials.swift
@@ -0,0 +1,23 @@
+
+import Foundation
+
+public protocol ISCCredentials {
+    var username: String { get }
+    var password: String { get }
+    var domain: String? { get }
+}
+
+public class SCCredentials: ISCCredentials {
+
+    public init (username: String, password: String, domain: String?)
+    {
+        self.username = username
+        self.password = password
+        self.domain = domain
+    }
+    
+    public let username: String
+    public let password: String
+    public let domain: String?
+
+}
